09389824b7b44e11b32c62e75380ca5ec93cb77e
Georgi Ivanov <3618084+georgiIvanov@users.noreply.github.com>
2024-04-11 14:44:25 +0300

    Add remappings

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@uniswapv3-periphery/interfaces/ISwapRouter.sol";
+import "@uniswapv3-core/interfaces/IUniswapV3Pool.sol";
+
 contract Counter {
     uint256 public number;
+    ISwapRouter public router;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
